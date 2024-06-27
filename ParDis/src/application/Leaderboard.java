9ee7182a83bd2965a02e5c978f35ecb592cab7f8
Joshua Encela <arellanopa@students.national-u.edu.ph>
2024-06-28 03:49:57 +0800

    "In Progress"

--- ParDis/src/application/Leaderboard.java
+++ ParDis/src/application/Leaderboard.java
@@ -1,8 +1,37 @@
-//package application;
-//
-//public class Leaderboard extends application{
-//
-//}
+package application;
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
-// WAHAHAHAHHAHAAHHAH
+public class Leaderboard extends Application {
+
+    Stage window;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+    	
+    	window = primaryStage;
+        window.setTitle("BRAINZZZ");
+
+        // GridPane
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setPadding(new Insets(20, 10, 10, 20));
+        grid.setVgap(5);
+        grid.setHgap(10);
+        
+        Scene scene = new Scene(grid, 960, 520);
+        scene.getStylesheets().add("loginStyle.css");
+        window.setScene(scene);
+        window.show();
+    }
+    
+}
